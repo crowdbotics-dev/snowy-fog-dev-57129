10f67a88f655710a4558213c09ad783f97f2bf43
Crowdbotics <engineering@crowdbotics.com>
2023-01-16 06:39:08 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "snowy-fog-dev-57129",
+  url: "https://snowy-fog-dev-57129.botics.co",
+  api: "https://snowy-fog-dev-57129.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
